cada617a530f6be61055ce4210cdc9b076e4bc94
houyonggang <1502492225@qq.com>
2020-10-15 13:40:01 +0800

    京东首页优化

--- js/slider.js
+++ js/slider.js
@@ -1,73 +1,160 @@
 class Slider {
-    constructor(id) {
+
+    constructor(id){
         this.box = document.querySelector(id)
         this.picBox = this.box.querySelector("ul")
         this.indexBox = this.box.querySelector(".index-box")
+
         this.sliderWidth = this.box.clientWidth
-        this.index =1
+        this.sliders = this.picBox.children.length
+        this.index = 1
+        this.animated = false
+        this.auto = null
+
         this.init()
     }
 
-    init() {
+    init(){
         this.initPoint()
         this.copyPic()
         this.leftRight()
+        this.play()
     }
 
-    initPoint() {
+    initPoint(){
         const num = this.picBox.children.length;
+
         let frg = document.createDocumentFragment();
-        for (let i = 0; i < num; i++) {
+
+        for(let i = 0; i < num; i++){
             let li = document.createElement("li")
-            li.setAttribute("data-index", i + 1)
-            if (i == 0) li.className = "active"
+            li.setAttribute("data-index", i+1)
+            if(i == 0) li.className = "active"
             frg.appendChild(li)
         }
+
         this.indexBox.children[0].style.width = num * 10 * 2 + "px";
+
         this.indexBox.children[0].appendChild(frg)
 
-        this.indexBox.children[0].addEventListener("click",(e)=>{
+        this.indexBox.children[0].addEventListener("click", (e) => {
+            console.log("point")
             let pointIndex = (e.target).getAttribute("data-index")
+            if(pointIndex == this.index || this.animated){
+                return
+            }
+
+            let offset = (pointIndex - this.index) * this.sliderWidth
             this.index = pointIndex
+            this.move(offset)
+
         })
     }
 
-    //使用辅助图实现轮播图无缝切换
     copyPic(){
         const first = this.picBox.firstElementChild.cloneNode(true)
         const last = this.picBox.lastElementChild.cloneNode(true)
         this.picBox.appendChild(first)
-        this.picBox.insertBeefore(last,this.picBox.firstElementChild)
+        this.picBox.insertBefore(last, this.picBox.firstElementChild)
 
-        this.picBox.style.width = this.sliderWidth * this.picBox.children.length +"px";
-        this.picBox.style.left = -1 * this.sliderWidth +"px";
+        this.picBox.style.width = this.sliderWidth * this.picBox.children.length + "px"
+        this.picBox.style.left = -1 * this.sliderWidth + "px"
     }
 
-    //移动距离
     move(offset){
-        this.animate(offset)
+        this.animate(offset);
+        
         const num = this.indexBox.children[0].children.length
-        for(let i =0;i<num;i++){
+        for(let i = 0; i < num; i++){
             this.indexBox.children[0].children[i].className = ""
         }
         this.indexBox.children[0].children[this.index-1].className = "active"
+
     }
 
-    //移动动画
     animate(offset){
+        const time = 1000
+        const rate = 100
+        let speed = offset / (time/rate)
+
+        let goal = parseFloat(this.picBox.style.left) - offset
+
+        this.animated = true
+
+        let animate = setInterval(() => {
+            if( this.picBox.style.left == goal || Math.abs(Math.abs(parseFloat(this.picBox.style.left)) - Math.abs(goal)) < Math.abs(speed) ){
+                this.picBox.style.left == goal
+                clearInterval(animate)
+                this.animated = false
+
+                if(parseFloat(this.picBox.style.left) == 0){
+                    this.picBox.style.left = -this.sliders * this.sliderWidth + "px"
+                }else if(parseFloat(this.picBox.style.left) == -(this.sliders+1)*this.sliderWidth){
+                    this.picBox.style.left = -this.sliderWidth + "px"
+                }
+            }else{
+                this.picBox.style.left = parseFloat(this.picBox.style.left) - speed + "px";
+            }
+        }, rate);
 
     }
 
-    //左右移动监听
     leftRight(){
 
-        this.box.querySelector(".left-box").addEventListener("click",()=>{
-            this.move(this.sliderWidth)
+        this.box.querySelector(".left-box").addEventListener("click", () => {
+            console.log("left")
+
+            if(this.animated){
+                return
+            }
+
+            if(this.index - 1 < 1){
+                this.index = this.sliders
+            }else{
+                this.index --
+            }
+
+            this.move(-this.sliderWidth)
+
         })
 
-        this.box.querySelector(".right-box").addEventListener("click",()=>{
-            this.move(-this.sliderWidth)
+        this.box.querySelector(".right-box").addEventListener("click", () => {
+            console.log("right")
+
+            if(this.animated){
+                return
+            }
+
+            if(this.index + 1 > this.sliders){
+                this.index = 1
+            }else{
+                this.index ++
+            }
+
+            this.move(this.sliderWidth)
+
+        })
+
+    }
+
+    play(){
+
+        this.auto = setInterval(() => {
+            this.box.querySelector(".right-box").click()
+        }, 2000);
+
+        this.box.addEventListener("mouseenter", () => {
+            clearInterval(this.auto)
+        })
+
+        this.box.addEventListener("mouseleave", () => {
+            this.auto = setInterval(() => {
+                this.box.querySelector(".right-box").click()
+            }, 2000);
         })
     }
 
 }
+
+
+
